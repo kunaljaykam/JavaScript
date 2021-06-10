56cd38662327e9dcb7b7d4a0fc783ccb2c9e1244
kunal jaykam <kunaljaykam@icloud.com>
2021-06-10 23:03:01 +0530

    js level 1

---
+++ app.js
@@ -0,0 +1,62 @@
+console.log("Hello");
+
+// comment line
+
+//variable
+let b = "titanic";
+console.log(b);
+
+//working with html in javascript
+
+//let music = prompt('what kinda music do you like');
+//document.getElementById("music").innerHTML = music;
+
+//numbers
+let num1 = 5.7;
+num1++;
+console.log(45 / 5);
+num1--;
+
+console.log(num1);
+
+//Functions
+function tion() {
+	console.log("this is tion functon");
+}
+
+tion();
+
+// function greet(yourLike) {
+// 	//let like = prompt('what do you like most?');
+// 	let result = "I love " + yourLike + " too";
+// 	console.log(result);
+// }
+
+// //let like = prompt('what do you like the most?')
+
+// greet(like);
+
+//adding two numbers in functions
+function addNumbers(num1, num2) {
+	let result = num1 + num2;
+	console.log(result);
+}
+
+addNumbers(34, 35);
+
+//while loops
+// let num = 0;
+
+// while (num < 100) {
+//     num += 1;
+//     console.log(num);
+// }
+
+// for loop
+// for (let num = 0; num <= 100; num++) {
+//     console.log(num);
+// }
+
+
+//Data types
+
